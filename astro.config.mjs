6f373510ce6e043cd9c0f3ccde8d243602cdd346
yretamalg <yerson.retamal@outlook.com>
2024-11-25 21:52:54 -0300

    netlify add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
