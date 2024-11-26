ab465b0a94513a3b5631490bcbd8c79b43b72fff
yretamalg <yerson.retamal@outlook.com>
2024-11-26 02:48:37 -0300

    Definir las tasas de retención con factores precisos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export const Footer = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const appVersion = '1.1';
+  const appVersion = '1.1.2';
 
   const openModal = (e) => {
     e.preventDefault();
