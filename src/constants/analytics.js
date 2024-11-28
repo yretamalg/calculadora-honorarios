28de10e680bc79eb5b6881c3df7c35a9ce4c1976
yretamalg <yerson.retamal@outlook.com>
2024-11-27 21:41:35 -0300

    Analytics

---
+++ src/constants/analytics.js
@@ -0,0 +1,4 @@
+export const ANALYTICS_CONFIG = {
+    GA_MEASUREMENT_ID: 'G-NKZ7YSQQ61',
+    // Puedes agregar más configuraciones de analytics aquí si las necesitas en el futuro
+};
--- src/constants/config.js
+++ src/constants/config.js
@@ -38,7 +38,7 @@ export const LIMITES = {
 
 export const APP_CONFIG = {
     nombre: 'vBox Pro',
-    version: '1.2.9',
+    version: '1.3.1',
     domain: 'https://www.vbox.pro',
     contactEmail: 'hola@vbox.pro',
     social: {
