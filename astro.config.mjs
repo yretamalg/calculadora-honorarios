912998e795a71023727659da0df33519b904f2ee
yretamalg <yerson.retamal@outlook.com>
2024-12-02 21:48:03 -0300

    new netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  adapter: netlify(),
+  site: 'https://honorario.netlify.app',
+  base: '/',
 });
