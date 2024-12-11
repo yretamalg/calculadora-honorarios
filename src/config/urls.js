70abbc94ca3e934ef1b1fa74c0d8ea09edc57e8d
yretamalg <yerson.retamal@outlook.com>
2024-12-11 03:06:55 -0300

    Update Privacy Policy and Terms of Use

---
+++ src/config/urls.js
@@ -0,0 +1,12 @@
+// src/config/urls.js
+export const EXTERNAL_URLS = {
+    SII_RETENCION: 'https://www.sii.cl/destacados/boletas_honorarios/aumenta_retencion.html',
+    BCN_LEY: 'https://www.bcn.cl/leychile/navegar?idNorma=1128420',
+    BANCO_CENTRAL: 'https://si3.bcentral.cl/Indicadoressiete/secure/Indicadoresdiarios.aspx'
+  };
+  
+  export const INTERNAL_URLS = {
+    PRIVACY: '/politica-de-privacidad',
+    TERMS: '/terminos-de-uso',
+    CHANGELOG: '/changelog'
+  };
